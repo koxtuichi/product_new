f9bf4aa114afce841ddaf0ae4b0af5d6278faba9
杉本光一 <sugimotokouichi@koichimac.lan>
2025-02-20 22:41:16 +0900

    カードコンポーネントを作成

---
+++ components/common/CardList.tsx
@@ -0,0 +1,46 @@
+// Card要素を使って、CardListを作成する
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { Card } from "./Card";
+
+// カードリストのコンポーネントを作成する
+// 水平方向にならべるために、ScrollViewを使う
+export const CardList = () => {
+  return (
+    <ScrollView horizontal style={styles.container}>
+      {/* Cardコンポーネントを配置する */}
+      <Card
+        title="Title1"
+        description="Description1"
+        imageSource={require("../../assets/images/image_sample.png")}
+      />
+      <Card
+        title="Title1"
+        description="Description1"
+        imageSource={require("../../assets/images/image_sample.png")}
+      />
+      <Card
+        title="Title1"
+        description="Description1"
+        imageSource={require("../../assets/images/image_sample.png")}
+      />
+      <Card
+        title="Title1"
+        description="Description1"
+        imageSource={require("../../assets/images/image_sample.png")}
+      />
+      <Card
+        title="Title1"
+        description="Description1"
+        imageSource={require("../../assets/images/image_sample.png")}
+      />
+    </ScrollView>
+  );
+};
+
+// スタイルを定義する
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
